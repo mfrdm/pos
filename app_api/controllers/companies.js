6ab248d1cd2e6c6995325b174707c05c38c51c0a
pakota159 <pakota159@gmail.com>
2017-03-30 18:29:46 +0700

    companies controllers API

--- app_api/controllers/companies.js
+++ app_api/controllers/companies.js
@@ -1,25 +1,33 @@
 var helper = require('../../libs/node/helper')
 var dbHelper = require('../../libs/node/dbHelper')
 var requestHelper = require('../../libs/node/requestHelper')
+var mongoose = require('mongoose');
+var CompaniesModel = mongoose.model('companies');
 
 module.exports = new Companies();
 
 function Companies() {
 
 	this.readSomeComps = function(req, res) {
-
+		var func = function(q){
+			q.conditions = {
+				name: "Gre"
+			}
+			return q;
+		}
+		dbHelper.findSome(req, res, CompaniesModel, func)
 	};
 
 	this.readOneCompById = function(req, res) {
-
+		dbHelper.findOneById(req, res, CompaniesModel, 'compid')
 	};
 
 	this.createOneComp = function(req, res) {
-
+		dbHelper.insertOne(req, res, CompaniesModel)
 	};
 
 	this.updateOneCompById = function(req, res) {
-
+		dbHelper.updateOneById(req, res, CompaniesModel, 'compid')
 	};
 
 };
--- app_api/models/companies.js
+++ app_api/models/companies.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var companiesSchema = mongoose.Schema({
-
+	name:{type:String}
 });
 
 mongoose.model ('companies', companiesSchema);
