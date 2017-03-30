3ce1fc43c1e432600e1a97aac73351a47f725800
pakota159 <pakota159@gmail.com>
2017-03-30 19:07:15 +0700

    orders controllers api

--- app_api/controllers/orders.js
+++ app_api/controllers/orders.js
@@ -1,25 +1,33 @@
 var helper = require('../../libs/node/helper')
 var dbHelper = require('../../libs/node/dbHelper')
 var requestHelper = require('../../libs/node/requestHelper')
+var mongoose = require('mongoose');
+var OrdersModel = mongoose.model('orders');
 
 module.exports = new Orders();
 
 function Orders() {
 
 	this.readSomeOrders = function(req, res) {
-
+		var func = function(q){
+			q.conditions = {
+				name: "food"
+			}
+			return q;
+		}
+		dbHelper.findSome(req, res, OrdersModel, func)
 	};
 
 	this.readOneOrderById = function(req, res) {
-
+		dbHelper.findOneById(req, res, OrdersModel, 'orderid')
 	};
 
 	this.createOneOrder = function(req, res) {
-
+		dbHelper.insertOne(req, res, OrdersModel)
 	};
 
 	this.updateOneOrderById = function(req, res) {
-
+		dbHelper.updateOneById(req, res, OrdersModel, 'orderid')
 	};
 
 };
--- app_api/models/orders.js
+++ app_api/models/orders.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 var ordersSchema = mongoose.Schema({
-
+	name:{type:String}
 });
 
 mongoose.model ('orders', ordersSchema);
