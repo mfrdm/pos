eaa04b8692bda492bbfc82d880998f30326c9537
pham hiep <pham hiep>
2017-04-17 17:31:15 +0700

    Modify adding new js files in layout

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -9,7 +9,13 @@ module.exports = new Others();
 function Others() {
 
 	this.angularApp = function (req, res){
-		var listFile = ['checkin', 'checkout', 'customers', 'assets', 'costs', 'home']
+		//var listFile = ['checkin', 'checkout', 'customers', 'assets', 'costs', 'home'];
+		var ctrFileNames = ['checkin', 'checkout', 'customers', 'assets', 'costs', 'home'];
+		var serviceFileNames = ['checkin', 'checkout', 'customers', 'assets', 'costs', 'home', 'common'];
+
+		var ctrlNum = ctrFileNames.length;
+		var serNum = serviceFileNames.length;
+
 		var data = {
 			user:{
 
@@ -20,10 +26,19 @@ function Others() {
 				js:['app.js'],
 			}
 		};
-		for (var i=0; i<listFile.length; i++){
-			data.look.js.push(listFile[i]+'.ctrl.js')
-			data.look.js.push('common/services/'+listFile[i]+'.service.js')
-		}			
+
+		for (var i=0; i < ctrlNum; i++){
+			data.look.js.push (ctrFileNames[i] + '.ctrl.js');
+		}
+
+		for (var i=0; i < serNum; i++){
+			data.look.js.push ('common/services/' + serviceFileNames[i] + '.service.js');
+		}
+
+		// for (var i=0; i<listFile.length; i++){
+		// 	data.look.js.push(listFile[i]+'.ctrl.js')
+		// 	data.look.js.push('common/services/'+listFile[i]+'.service.js')
+		// }			
 
 		res.render ('layout', {data: data});
 	}
