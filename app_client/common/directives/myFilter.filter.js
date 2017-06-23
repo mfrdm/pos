f505baf290afa6f45fe12d01ba60ecb130060bb6
pakota159 <pakota159@gmail.com>
2017-06-23 14:47:08 +0700

    add pagination product and storage list, need to optimize later

--- app_client/common/directives/myFilter.filter.js
+++ app_client/common/directives/myFilter.filter.js
@@ -3,6 +3,7 @@ angular
 	.filter('myfilter', [myfilter])
 	.filter('filterName', [filterName])
     .filter('filterCategory', [filterCategory])
+    .filter('filterByName', [filterByName])
 
 var defaultDiacriticsRemovalMap = [
     {'base':'A', 'letters':'\u0041\u24B6\uFF21\u00C0\u00C1\u00C2\u1EA6\u1EA4\u1EAA\u1EA8\u00C3\u0100\u0102\u1EB0\u1EAE\u1EB4\u1EB2\u0226\u01E0\u00C4\u01DE\u1EA2\u00C5\u01FA\u01CD\u0200\u0202\u1EA0\u1EAC\u1EB6\u1E00\u0104\u023A\u2C6F'},
@@ -151,5 +152,16 @@ function filterCategory(){
         }else{
             return item.filter(function(ele){return ele.category == condition})
         }
+    }
+}
+
+function filterByName(){
+    return function(item, condition){
+        return item.filter(function(ele){
+            if(condition){
+                return ele.name.includes(condition)
+            }
+            return ele
+        })
     }
 }
